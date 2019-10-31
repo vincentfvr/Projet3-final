0a834b85a0f32585bee4114f28baf027ab653944
Vincent Favre <vincent.gw16@gmail.com>
2019-10-31 09:37:32 +0100

    Backup avant ajout du control de saisie choix du mode et replay

--- src/com/ocr/vincent/Game.java
+++ src/com/ocr/vincent/Game.java
@@ -10,7 +10,7 @@ public class Game {
     /**
      * LANCEMENT DU JEU : Choix du mode
      */
-    public void play() {
+    void play() {
         int mode; // Choix du mode de jeu  1 ou 2 ou 3
         Scanner sc = new Scanner(System.in);
         System.out.println("[NOUVELLE PARTIE]");
@@ -19,27 +19,27 @@ public class Game {
                 System.out.println("CHOISIR UN MODE DE JEU : 1 - CHALLENGER, 2 - DÉFENSEUR, 3 - DUEL");
                 if (Settings.devMode) System.out.println("[MODE DÉVELOPPEUR ACTIF]");
                 mode = sc.nextInt();
-                /** Lancement du mode de jeu choisi */
+                // Lancement du mode de jeu choisi
                 switch (mode) {
-                    case 1: /** MODE CHALLENGER */
+                    case 1: // MODE CHALLENGER
                         System.out.println("[MODE CHALLENGER] : Trouver la bonne combinaison ");
                         logger.info("[MODE CHALLENGER] defender.run");
                         Challenger challenger = new Challenger();
                         challenger.run("USER", Settings.combLen);
                         break;
-                    case 2: /** MODE DEFENSEUR */
+                    case 2: // MODE DEFENSEUR
                         System.out.println("[MODE DÉFENSEUR] : Définir une combinaison secrète");
                         logger.info("[MODE DÉFENSEUR] defender.run");
                         Defender defender = new Defender();
                         defender.run("CPU", Settings.combLen);
                         break;
-                    case 3: /** MODE DUEL */
+                    case 3: // MODE DUEL
                         System.out.println("[MODE DUEL]");
                         logger.info("[MODE DUEL] dual.run");
                         Dual dual = new Dual();
                         dual.run(Settings.combLen);
                         break;
-                    case 123: /** "123" active le mode Développeur false par defaut dans le .properties */
+                    case 123: // "123" active le mode Développeur false par defaut dans le .properties
                         //System.out.println("[MODE DÉVELOPPEUR ACTIF]");
                         Settings.devMode = true;
                         logger.info("[MODE DÉVELOPPEUR ACTIF]");
@@ -56,14 +56,14 @@ public class Game {
      * Méthode permettant de relancer une partie ou quitter
      * @return b1 True = Rejouer, False = Quitter le jeu
      */
-    public boolean replay () {
+    boolean replay() {
 
         Scanner sc = new Scanner(System.in);
         Controls controls = new Controls();
         boolean b1 = true; // rue -> Rejouer, False = Quitter le jeu
-        boolean b2 = false; // Controle de saisie True -> erreur, false -> pas d'erreur
+        boolean b2 ; // Controle de saisie True -> erreur, false -> pas d'erreur
         int myChoise; // Choix 1 ou 2 pour rejouer ou quitter
-        String myChoiseStr = ""; // Choix au format string pour le control de saisie
+        String myChoiseStr; // Choix au format string pour le control de saisie
 
         do {
             System.out.println("Rejouer une partie ? : Taper : 1 -> oui ou 2 -> non");
@@ -82,10 +82,7 @@ public class Game {
                         break;
                 }
             }
-            /**if (myChoiseStr.equals("1") == true || myChoiseStr.equals("2") == true) {
-                b2 = true;
-            }*/
-        } while (!myChoiseStr.equals("1") && !myChoiseStr.equals("2"));
+        } while (!myChoiseStr.equals("1") && !myChoiseStr.equals("2")); //(!b2)
         return b1;
     }
 }
